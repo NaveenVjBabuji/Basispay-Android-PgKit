58e2023ca773240ee469e72f18a1e9af6508a84b
NaveenVjBabuji <naveenkumar.v@basispay.in>
2021-07-07 15:59:42 +0530

    sample check

--- PaymentGateway/src/main/java/com/example/paymentgateway/Sample1View.java
+++ PaymentGateway/src/main/java/com/example/paymentgateway/Sample1View.java
@@ -1,5 +1,12 @@
 package com.example.paymentgateway;
 
-public class Sample1View {
+import com.test.pg.secure.pgsdkv4.PaymentParams;
 
+public class Sample1View {
+  PaymentParams params;
+
+
+  void initiateGateway(){
+      params.getAddressLine1();
+  }
 }
