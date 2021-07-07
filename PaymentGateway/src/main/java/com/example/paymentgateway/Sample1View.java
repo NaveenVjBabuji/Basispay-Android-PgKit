35f41f0d9c1c4a4228af3b8fec96abf54354d46c
NaveenVjBabuji <naveenkumar.v@basispay.in>
2021-07-07 16:04:54 +0530

    temp commit

--- PaymentGateway/src/main/java/com/example/paymentgateway/Sample1View.java
+++ PaymentGateway/src/main/java/com/example/paymentgateway/Sample1View.java
@@ -3,10 +3,9 @@ package com.example.paymentgateway;
 import com.test.pg.secure.pgsdkv4.PaymentParams;
 
 public class Sample1View {
-  PaymentParams params;
-
-
-  void initiateGateway(){
+  public static PaymentParams params;
+  
+    public void initiateGateway(){
       params.getAddressLine1();
   }
 }
